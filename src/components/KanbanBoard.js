4e097784be8b576780fcd3fb3b9a1968ea44c2c1
Pranjal Kumar <pranjalk449@gmail.com>
2024-09-20 01:19:02 +0530

    fixing header css

--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -15,18 +15,15 @@ function KanbanBoard({ tickets, grouping, users, sorting }) {
     <div className="kanban-board">
       {Object.keys(sortedGroupedTickets).map(group => (
         <div key={group} className="kanban-column">
-           <div>
             <div className="kanban-column-header">
-              {/* Display the number of tickets inline with the title */}
               <div className="kanban-card-count">
-                <h2 className="kanban-column-title">{grouping === 'priority' ? priorityMap[group] : group} {/* Show priority name or group */}</h2>
-                <h5 className='kanban-column-count'>{groupedTickets[group].length}</h5>
+                <div className="kanban-column-title">{grouping === 'priority' ? priorityMap[group] : group} {/* Show priority name or group */}</div>
+                <div className='kanban-column-count'>{groupedTickets[group].length}</div>
               </div>
               <div class="added">
                 <img src={`${process.env.PUBLIC_URL}/icons_FEtask/add.svg`} alt="add"/>
                 <img src={`${process.env.PUBLIC_URL}/icons_FEtask/3 dot menu.svg`} alt="3 dot menu"/>
               </div>
-            </div>
             </div>
           <div className="kanban-cards">
             {sortedGroupedTickets[group].map(ticket => (
